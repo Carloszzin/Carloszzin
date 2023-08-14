0b0df0a13b79dc2fe09d9b2bbe3d22f1df972a72
Carloszzin <139806232+Carloszzin@users.noreply.github.com>
2023-08-14 14:04:11 -0300

    Praticando

---
+++ acoes.js
@@ -0,0 +1,18 @@
+// Escopo global
+let s = document.querySelector('section#saida')
+
+function c1() {
+    s.innerHTML += '<p>Você clicou no <strong>primeiro</strong> botão!</p>'
+}
+
+function c2() {
+    s.innerHTML += '<p>Você clicou no <strong>segundo</strong> botão!</p>'
+}
+
+function c3() {
+    s.innerHTML += '<p>Você clicou no <strong>terceiro</strong> botão!</p>'
+}
+
+function c4() {
+    s.innerHTML += '<p>Você clicou no <strong>quarto</strong> botão!</p>'
+}
